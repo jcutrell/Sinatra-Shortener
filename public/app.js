6c7c795ade076dd18dd01b28262f8584ef0ba80a
Jonathan Cutrell <jonathan.cutrell@thisismedium.com>
2011-10-27 12:15:11 -0400

    iphone js

--- public/app.js
+++ public/app.js
@@ -3,4 +3,11 @@
         $("input:text").focus(function() { $(this).select(); } );
     }).mouseup(function(e){e.preventDefault();});
     $("table").dataTable();
+    var updateLayout = function() {
+        if (window.innerWidth != currentWidth) {
+            currentWidth = window.innerWidth; var orient = (currentWidth == 320) ? "profile" : "landscape"; document.body.setAttribute("orient", orient);
+            window.scrollTo(0, 1);
+            }
+            }; iPhone.DomLoad(updateLayout); setInterval(updateLayout, 500); 
+    
 })(jQuery);
